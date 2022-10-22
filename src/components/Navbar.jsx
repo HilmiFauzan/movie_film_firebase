a97eda9183a768ac0b98ef0da9f32af03158e66e
Hilmi Fauzan <hilmifauzan170@gmail.com>
2022-10-22 22:05:08 +0700

    this app movie-film react for review movie

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+const Navbar = () => {
+	const {user, logOut} = UserAuth();
+	const navigate = useNavigate();
+	// console.log(user.email)
+
+	const handleLogout = async () => {
+		try {
+			await logOut()
+			navigate('/');
+		} catch(error) {
+			// statements
+			console.log(error);
+		}
+	};
+
+	return(
+		<div className="w-full flex items-center justify-between p-4 z-[100] absolute">
+			<Link to='/'>
+				<h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+					MOV-FI
+				</h1>
+			</Link>
+			{
+				user?.email ? 
+				<div>
+					<Link to="/account">
+						<button className="text-white pr-4">
+							Account
+						</button>
+					</Link>
+					<button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+						Log Out
+					</button>
+				</div>
+				:
+				<div>
+					<Link to="/login">
+						<button className="text-white pr-4">
+							Sign In
+						</button>
+					</Link>
+					<Link to="/signup">
+						<button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+							Sign Up
+						</button>
+					</Link>
+				</div>
+			}
+		</div>
+	)
+}
+
+export default Navbar
---
+++ src/pages/Account.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SavedShows from '../components/SavedShows'
+
+const Account = () => {
+	return (
+		<>
+			<div className="w-full text-white">
+				<img className="w-full h-[400px] object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/b321426e-35ae-4661-b899-d63bca17648a/4eb4e01e-2723-438e-bf4a-a17a19760663/ID-en-20220926-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+				<div className="bg-black/60 fixed top-0 w-full h-[550px]"></div>
+				<div className="absolute top-[20%] p-4 md:p-8">
+					<h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+				</div>
+			</div>
+			<SavedShows />
+		</>
+	)
+}
+
+export default Account
